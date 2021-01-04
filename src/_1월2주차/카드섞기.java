b49f66aa20eef98079cb5b19c055763669883c0e
jeff-seyong <sedragon.kim@gmail.com>
2021-01-05 00:02:00 +0900

    Add break condition(isCircle method)

--- src/_1월2주차/카드섞기.java
+++ src/_1월2주차/카드섞기.java
@@ -11,13 +11,16 @@ public class 카드섞기 {
         int N = Integer.parseInt(br.readLine());
 
         int[] P = new int[49 * 2];
+        int[] origin = new int[49 * 2];
         int[] S = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) P[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++)
+            P[i] = origin[i] = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) S[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++)
+            S[i] = Integer.parseInt(st.nextToken());
 
         int answer = 0;
         while (true) {
@@ -26,12 +29,21 @@ public class 카드섞기 {
             shuffle(P, S, N);
             answer++;
 
-            if (answer > 1000000) {
+            if (isCircle(origin, P, N)) {
                 answer = -1;
                 break;
             }
         }
         System.out.println(answer);
+    }
+
+    private static boolean isCircle(int[] origin, int[] P, int N) {
+        for (int i = 0; i < N; i++) {
+            if (origin[i] != P[i]){
+                return false;
+            }
+        }
+        return true;
     }
 
     private static void shuffle(int[] P, int[] S, int N) {
