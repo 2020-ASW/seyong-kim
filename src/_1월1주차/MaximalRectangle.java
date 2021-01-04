dae3e3fee853449e54099fb1110a836623bd868d
jeff-seyong <sedragon.kim@gmail.com>
2021-01-04 12:39:26 +0900

    Add comment

--- src/_1월1주차/MaximalRectangle.java
+++ src/_1월1주차/MaximalRectangle.java
@@ -2,6 +2,21 @@ package _1월1주차;
 
 import java.util.Stack;
 
+/*
+시간 복잡도 : O(n*m)
+1. 배열의 row[0]는 arr에 그대로 copy
+2. maxArea에 arr에서 얻을 수있는 최대 넓이를 저장
+3. row[1]를 봤을때
+    if row[1][i] == 1 : arr[i] += 1
+    else row[1][i] == 0 : arr[i] = 0
+4. arr에서 얻을 수있는 최대 넓이를 구한 뒤 갱신
+5. 3~4번 row 끝날 때까지 반복
+
+참고
+- https://www.youtube.com/watch?v=g8bSdXCG-lA&ab_channel=TusharRoy-CodingMadeSimple
+- https://leetcode.com/problems/maximal-rectangle/discuss/29055/My-java-solution-based-on-Maximum-Rectangle-in-Histogram-with-explanation
+* */
+
 public class MaximalRectangle {
     public static int maximalRectangle(char[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) return 0;
@@ -53,15 +68,3 @@ public class MaximalRectangle {
         System.out.println(maximalRectangle(matrix));
     }
 }
-
-
-/*
-시간 복잡도 : O(n*m)
-1. 배열의 row[0]는 arr에 그대로 copy
-2. maxArea에 arr에서 얻을 수있는 최대 넓이를 저장
-3. row[1]를 봤을때
-    if row[1][i] == 1 : arr[i] += 1
-    else row[1][i] == 0 : arr[i] = 0
-4. arr에서 얻을 수있는 최대 넓이를 구한 뒤 갱신
-5. 3~4번 row 끝 날때까지 반복
-* */
