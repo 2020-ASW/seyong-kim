31fe39299728e68e03c214546e34e2ea401498d9
jeff-seyong <sedragon.kim@gmail.com>
2021-01-06 17:28:26 +0900

    210106

---
+++ src/_1월2주차/NumberOfIslands.java
@@ -0,0 +1,51 @@
+package _1월2주차;
+
+import java.util.*;
+
+public class NumberOfIslands {
+    static int[][] map;
+    static int[][] direction = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    static int N, M;
+
+    public static int numIslands(char[][] grid) {
+        map = new int[grid.length][grid[0].length];
+        N = grid.length;
+        M = grid[0].length;
+        int sector = 1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0 && grid[i][j] == '1') {
+                    bfs(grid, i, j, sector);
+                    sector++;
+                }
+            }
+        }
+        return sector-1;
+    }
+
+    private static void bfs(char[][] grid, int x, int y, int sector) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+        map[x][y] = sector;
+
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+
+            for (int[] dir : direction) {
+                int nx = pos[0] + dir[0];
+                int ny = pos[1] + dir[1];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+
+                if (map[nx][ny] != 0) continue;
+
+                if (grid[nx][ny] == '1') {
+                    map[nx][ny] = sector;
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+    }
+}
--- src/_1월2주차/경주로건설.java
+++ src/_1월2주차/경주로건설.java
@@ -1,53 +1,89 @@
 package _1월2주차;
 
-import java.util.Arrays;
+import java.util.*;
 
-// Fail 78.3/100 (not passed  5 test case)
+// (1) 실패 78.3/100 (not passed 5 test case)
+// (2) dfs에서 bfs로 변경
+// (3) 이미 설계된 곳이라면 값 비교 후, 최소 비용 저장
 
 public class 경주로건설 {
     static int N, answer;
     static int[][] direction = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-    static boolean[][] visited;
-    static int[][] dp;
 
     public static int solution(int[][] board) {
         answer = Integer.MAX_VALUE;
         N = board.length;
-        visited = new boolean[N][N];
-        dfs(board, 0, 0, 0, 0);
 
-        for (int[] row : dp){
-            System.out.println(Arrays.toString(row));
-        }
+        bfs(board, new Car(0, 0, 0, -1));
 
         return answer;
     }
 
-    private static void dfs(int[][] board, int x, int y, int cost, int preDir) {
-        if (x == N - 1 && y == N - 1) {
-            answer = Math.min(answer, cost);
-            return;
-        }
-        // 현재 cost가 answer보다 커지면 볼필요 없음
-        if(cost >= answer) return;
+    private static void bfs(int[][] board, Car start) {
+        Queue<Car> queue = new LinkedList<>();
+        board[start.x][start.y] = 1;
+        queue.add(start);
 
-        for (int d = 1; d <= direction.length; d++) {
-            int nx = x + direction[d - 1][0];
-            int ny = y + direction[d - 1][1];
+        while (!queue.isEmpty()) {
+            Car cur = queue.poll();
 
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
-
-            if (board[nx][ny] == 1) continue;
-
-            visited[nx][ny] = true;
-            int add = 100;
-
-            if (preDir != 0 && preDir != d) {
-                add += 500;
+            if (cur.x == N - 1 && cur.y == N - 1) {
+                answer = Math.min(answer, cur.cost);
+                continue;
             }
-            dfs(board, nx, ny, cost + add, d);
 
-            visited[nx][ny] = false;
+            for (int d = 0; d < direction.length; d++) {
+                int nx = cur.x + direction[d][0];
+                int ny = cur.y + direction[d][1];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N || board[nx][ny] == 1) continue;
+
+                int newCost = cur.cost;
+
+                if (cur.preDir == -1 || cur.preDir == d) {
+                    newCost += 100;
+                } else { // 다른 방향으로 갈 경우(회전하는 경우만)
+                    newCost += 100 + 500;
+                }
+
+                if (board[nx][ny] == 0) {
+                    board[nx][ny] = newCost;
+                    queue.add(new Car(nx, ny, newCost, d));
+                }
+                else if(board[nx][ny] >= newCost){
+                    board[nx][ny] = Math.min(board[nx][ny], newCost);
+                    queue.add(new Car(nx, ny, newCost, d));
+                }
+
+            }
         }
+    }
+
+    static class Car {
+        int x, y, cost, preDir;
+
+        Car(int x, int y, int cost, int preDir) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+            this.preDir = preDir;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + x + "," + y + "]" + cost + "/" + preDir;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] board = new int[][]
+                {
+                        {0, 0, 0, 0, 1},
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 1, 0, 0},
+                        {0, 1, 0, 0, 1},
+                        {1, 0, 0, 0, 0},
+                };
+        System.out.println(solution(board));
     }
 }
