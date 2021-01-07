a080d2a07914d46ace7b7a1a5afc20a32f50cbc1
jeff-seyong <sedragon.kim@gmail.com>
2021-01-07 18:25:16 +0900

    Clean code and add comment

--- src/_1월1주차/치킨배달.java
+++ src/_1월1주차/치킨배달.java
@@ -7,9 +7,8 @@ import java.util.*;
 
 
 public class 치킨배달 {
-    static int N, M;
+    static int N, M, result;
     static int[][] map;
-    static boolean[][] valid;
     static List<int[]> houseList;
     static List<int[]> chickenHouseList;
     static List<int[]> selected;
@@ -17,12 +16,11 @@ public class 치킨배달 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
 
+        N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N + 1][N + 1];
-        valid = new boolean[N + 1][N + 1];
 
         houseList = new ArrayList<>();
         chickenHouseList = new ArrayList<>();
@@ -30,8 +28,11 @@ public class 치킨배달 {
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
+
             for (int j = 1; j <= N; j++) {
+
                 map[i][j] = Integer.parseInt(st.nextToken());
+
                 if (map[i][j] == 1) {
                     houseList.add(new int[]{i, j});
                 } else if (map[i][j] == 2) {
@@ -46,11 +47,11 @@ public class 치킨배달 {
         System.out.println(result);
     }
 
-    static int result;
+
 
     private static void selectChickenHouse(int start, int cnt) {
         if (cnt == M) {
-            // 도시의 치킨 거리
+            // 도시의 치킨 거리 중 최소값을 구한다
             int distance = calculate();
             result = Math.min(result, distance);
             return;
@@ -63,13 +64,15 @@ public class 치킨배달 {
         }
     }
 
+    // 집을 기준으로 선택된 M개의 치킨집과의 거리를 구한 뒤 모두 더해 도시의 치킨 거리를 구한다
     private static int calculate() {
+        // 도시의 치킨 거리
         int sum = 0;
-        for (int[] home : houseList) {
+        for (int[] house : houseList) {
             int min = Integer.MAX_VALUE;
 
             for (int[] chickenHouse : selected) {
-                int distance = getDistance(home[0], home[1], chickenHouse[0], chickenHouse[1]);
+                int distance = getDistance(house[0], house[1], chickenHouse[0], chickenHouse[1]);
                 min = Math.min(min, distance);
             }
 
