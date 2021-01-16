7d53397dc91f9bfa3edf7c5132a80cbb09a631d5
jeff-seyong <sedragon.kim@gmail.com>
2021-01-17 02:42:24 +0900

    Fail

---
+++ src/_1월3주차/파티.java
@@ -0,0 +1,89 @@
+package _1월3주차;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 파티 {
+    static int N, M, X;
+    static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
+
+        ArrayList<Node>[] map = new ArrayList[N + 1];
+        for (int i = 0; i < map.length; i++) map[i] = new ArrayList<>();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            map[u].add(new Node(v, t));
+        }
+
+        int[] time = dijkstra(map, X);
+
+        int answer = 0, student = 0;
+        for (int i = 1; i < time.length; i++) {
+            if (time[i] != INF && answer < time[i]) {
+                answer = time[i];
+                student = i;
+            }
+        }
+
+        time = dijkstra(map, student);
+        answer += time[X];
+
+        System.out.println(answer);
+    }
+
+    private static int[] dijkstra(ArrayList<Node>[] map, int party) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(party, 0));
+
+        int[] dist = new int[map.length];
+        Arrays.fill(dist, INF);
+
+        dist[party] = 0;
+
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (now.time > dist[now.no]) continue;
+
+            for (Node next : map[now.no]) {
+                if (dist[next.no] > dist[now.no] + next.time) {
+                    dist[next.no] = dist[now.no] + next.time;
+                    pq.add(new Node(next.no, dist[next.no]));
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    static class Node implements Comparable<Node> {
+        int no, time;
+
+        Node(int no, int time) {
+            this.no = no;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.time - o.time;
+        }
+    }
+}
