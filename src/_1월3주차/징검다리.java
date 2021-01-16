6bcf540c9dc3c01e1602001c3539c6c56f6539ab
jeff-seyong <sedragon.kim@gmail.com>
2021-01-16 17:38:28 +0900

    Solved (separate method)

--- src/_1월3주차/징검다리.java
+++ src/_1월3주차/징검다리.java
@@ -11,28 +11,33 @@ public class 징검다리 {
 
         while (l <= r) {
             int mid = (l + r) / 2;      // 각 징검다리 사이 거리의 최소값 중 가장 큰 값
-            int remove = 0;             // 제거한 징검다리의 수
-
-            // 각 지점 사이 거리 확인
-            int preRock = 0;
-            for (int i = 0; i < rocks.length; i++) {
-                if (rocks[i] - preRock < mid)   // 기대하는 값보다 지점 사이의 거리가 작으면 해당 돌 제거
-                    remove++;
-                else                            // 사이 거리를 계산할 기준 지점 갱신
-                    preRock = rocks[i];
-            }
-            // 마지막 지점과 도착 지점 사이 거리
-            if (distance - rocks[rocks.length - 1] < mid) remove++;
 
             // n 보다 제거한 돌이 많은 경우 = mid 값을 너무 크게 잡았기 때문
-            if (remove > n)
-                r = mid - 1;
-            else {
+            if (isMaxDistance(mid, n, rocks, distance)) {
                 answer = Math.max(answer, mid);
                 l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
 
         return answer;
+    }
+
+    private boolean isMaxDistance(int target, int numOfRocks, int[] rocks, int distance) {
+        int preRock = 0;
+
+        for (int i = 0; i < rocks.length; i++) {
+            if (rocks[i] - preRock < target)   // 기대하는 값보다 지점 사이의 거리가 작으면 해당 돌 제거
+                numOfRocks--;
+            else                            // 사이 거리를 계산할 기준 지점 갱신
+                preRock = rocks[i];
+
+            if (numOfRocks < 0) return false;
+        }
+        // 마지막 지점과 도착 지점 사이 거리
+        if (distance - rocks[rocks.length - 1] < target) numOfRocks--;
+        
+        return numOfRocks >= 0;
     }
 }
