e252c87f69521ce39e87b5b883871981f8f80318
jeff-seyong <sedragon.kim@gmail.com>
2021-02-07 20:06:27 +0900

    Failed (Null case)

---
+++ src/_2월1주차/MergeKSortedLists.java
@@ -0,0 +1,63 @@
+package _2월1주차;
+
+
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists {
+    public static ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (ListNode node : lists) {
+            if (node == null) continue;
+            pq.add(node.val);
+
+            ListNode next = node.next;
+            while (next != null) {
+                pq.add(next.val);
+                next = next.next;
+            }
+        }
+        ListNode head = new ListNode();
+        ListNode cur = head;
+
+        while (!pq.isEmpty()) {
+            cur.val = pq.poll();
+            cur.next = (pq.peek() != null ? new ListNode(pq.peek()) : null);
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        node1.next = new ListNode(4);
+        node1.next.next = new ListNode(5);
+
+        ListNode node2 = new ListNode(1);
+        node2.next = new ListNode(3);
+        node2.next.next = new ListNode(4);
+
+        ListNode node3 = new ListNode(2);
+        node3.next = new ListNode(6);
+
+        ListNode[] list = {node1, node2, node3};
+//        mergeKLists(list);
+        mergeKLists(new ListNode[]{null});
+    }
+}
