6c9752de1112be656d586302bf657864ff7b0a04
jeff-seyong <sedragon.kim@gmail.com>
2021-02-13 21:07:08 +0900

    Solved

--- src/_2월2주차/KthSmallestElementInASortedMatrix.java
+++ src/_2월2주차/KthSmallestElementInASortedMatrix.java
@@ -1,10 +1,21 @@
 package _2월2주차;
 
+import java.util.PriorityQueue;
+
 public class KthSmallestElementInASortedMatrix {
     public int kthSmallest(int[][] matrix, int k) {
-        int row = (k - 1) / matrix[0].length;
-        int col = (k - 1) % matrix[0].length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        return matrix[row][col];
+        for (int[] row : matrix){
+            for(int x : row){
+                pq.add(x);
+            }
+        }
+        int idx = 1;
+        while(!pq.isEmpty()){
+            int result = pq.poll();
+            if(idx++ == k) return result;
+        }
+        return 0;
     }
 }
