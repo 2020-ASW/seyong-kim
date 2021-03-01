dd8deab1fca75aef537fecce73fe81b43fa58485
jeff-seyong <sedragon.kim@gmail.com>
2021-03-01 16:04:51 +0900

    Solved

---
+++ src/_3월1주차/입국심사.java
@@ -0,0 +1,34 @@
+package _3월1주차;
+
+import java.util.Arrays;
+
+class 입국심사 {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+
+        long max = times[times.length - 1];
+
+        long answer = max * n;
+        long left = 1, right = max * n;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            if (isPossible(times, n, mid)) {
+                answer = Math.min(answer, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return answer;
+    }
+
+    private boolean isPossible(int[] times, int n, long mid) {
+        long count = 0;
+        for (int time : times) {
+            count += mid / time;
+        }
+        return count >= n;
+    }
+}
